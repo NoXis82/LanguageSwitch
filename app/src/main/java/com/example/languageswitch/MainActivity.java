96dda93955f59e6b6bb570937309112c803cd198
Slava Tumanov <vn.tumanov@me.com>
2020-05-30 22:57:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/languageswitch/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.languageswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void initView() {
+        Button button = findViewById(R.id.button);
+        final Spinner spinner = findViewById(R.id.spinner_language);
+        button.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                switch (spinner.getSelectedItemPosition()) {
+                    case 0:
+                        changeLocale("ru");
+                        break;
+                    case 1:
+                        changeLocale("en");
+                        break;
+                    case 2:
+                        changeLocale("de");
+                }
+            }
+        });
+    }
+
+    public void changeLocale(String language) {
+        Locale locale = new Locale(language);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext()
+                .getResources()
+                .getDisplayMetrics());
+        recreate();
+    }
+}
